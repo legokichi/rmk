d5853c6ce04a5e33fecc355fc7d8893e9d687918
Legokichi Duckscallion <legokichi@gmail.com>
2026-01-15 19:39:04 +0900

    Slow down CYW43 PIO SPI clock for RP2350

--- rmk-macro/src/chip_init.rs
+++ rmk-macro/src/chip_init.rs
@@ -170,7 +170,8 @@ pub(crate) fn chip_init_default(keyboard_config: &KeyboardTomlConfig, peripheral
                     let spi = ::cyw43_pio::PioSpi::new(
                         &mut pio.common,
                         pio.sm0,
-                        ::cyw43_pio::DEFAULT_CLOCK_DIVIDER,
+                        // Slow down SPI clock to avoid RP2350 input timing issues.
+                        ::cyw43_pio::RM2_CLOCK_DIVIDER * 2,
                         pio.irq0,
                         cs,
                         p.PIN_24,
