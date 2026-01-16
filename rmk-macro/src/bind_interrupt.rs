273c83141eb61a6a36ae87a7c908a0043faa768e
Legokichi Duckscallion <legokichi@gmail.com>
2026-01-16 15:39:34 +0900

    Allow Rp2040 build without USB IRQ

--- rmk-macro/src/bind_interrupt.rs
+++ rmk-macro/src/bind_interrupt.rs
@@ -173,9 +173,15 @@ pub(crate) fn bind_interrupt_default(keyboard_config: &KeyboardTomlConfig, item_
             }
         }
         rmk_config::ChipSeries::Rp2040 => {
-            let usb_info = communication.get_usb_info().expect("no usb info for the chip");
-            let interrupt_name = format_ident!("{}", usb_info.interrupt_name);
-            let peripheral_name = format_ident!("{}", usb_info.peripheral_name);
+            let usb_irq = if let Some(usb_info) = communication.get_usb_info() {
+                let interrupt_name = format_ident!("{}", usb_info.interrupt_name);
+                let peripheral_name = format_ident!("{}", usb_info.peripheral_name);
+                quote! {
+                    #interrupt_name => ::embassy_rp::usb::InterruptHandler<::embassy_rp::peripherals::#peripheral_name>;
+                }
+            } else {
+                quote! {}
+            };
             // For Pico W, enabled PIO0_IRQ_0 interrupt
             let (pio0_irq_0, ble_task) = if communication.ble_enabled() {
                 (
@@ -192,13 +198,18 @@ pub(crate) fn bind_interrupt_default(keyboard_config: &KeyboardTomlConfig, item_
             } else {
                 (quote! {}, quote! {})
             };
-            quote! {
-                use ::embassy_rp::bind_interrupts;
-                bind_interrupts!(struct Irqs {
-                    #interrupt_name => ::embassy_rp::usb::InterruptHandler<::embassy_rp::peripherals::#peripheral_name>;
-                    #pio0_irq_0
-                });
-                #ble_task
+            if usb_irq.is_empty() && pio0_irq_0.is_empty() && extern_irqs_vec.is_empty() {
+                quote! {}
+            } else {
+                quote! {
+                    use ::embassy_rp::bind_interrupts;
+                    bind_interrupts!(struct Irqs {
+                        #usb_irq
+                        #pio0_irq_0
+                        #extern_irqs
+                    });
+                    #ble_task
+                }
             }
         }
         rmk_config::ChipSeries::Esp32 => quote! {},
